d8d322da2ed41e23aa1a013a561ca3ec1013a68d
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-30 16:15:49 +0900

    グローバルスタイル適用

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { StyledButton } from './Components/Button.style';
 import { AppContainer } from './Components/Container.style';
+import { GlobalStyles } from './Components/GlobalSytles.style';
 
 function App() {
   return (
     <AppContainer>
+      <GlobalStyles />
       <StyledButton buttonLabel="Click Here" background="violet"></StyledButton>
     </AppContainer>
   );
---
+++ src/Components/GlobalSytles.style.jsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: indigo;
+    margin: 0;
+    padding: 0;
+  }
+`;
