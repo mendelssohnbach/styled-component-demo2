a37d472a95289e1b0c0040189cf02316b7ae03cf
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-30 14:14:54 +0900

    3色ボタンコンポーネント

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { RedButton, GreenButton, BlueButton } from './Components/Button.style';
+
 function App() {
-  return <div className="App">Leave a Like and Subscribe</div>;
+  return (
+    <div className="App">
+      <RedButton>Click this Button!</RedButton>
+      <BlueButton>Click this Button!</BlueButton>
+      <GreenButton>Click this Button!</GreenButton>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Button.style.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const BlueButton = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: blue;
+`;
+
+export const GreenButton = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: green;
+`;
+
+export const RedButton = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: red;
+`;
